01821db35cfc3b7ba204a1a15aea6412f8434a8f
rahul kumar <rahul.kumar@ihub-data.iiit.ac.in>
2024-05-16 11:12:30 +0530

    minor changes

--- src/components/Home/Navbar/Topbar.js
+++ src/components/Home/Navbar/Topbar.js
@@ -1,5 +1,4 @@
 import "./topbar.css";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -20,7 +19,7 @@ import logo from '../../assets/main/SITAMARHI1.png'
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="m-auto my-2 my-lg-0"
+            className="mx-auto my-2 my-lg-0"
             navbarScroll
           >
             <Nav.Link className='navbar-link'><Link to="/">Home</Link></Nav.Link>
